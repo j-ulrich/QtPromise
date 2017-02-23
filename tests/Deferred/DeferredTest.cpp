08352819e8e0aaf70492ce3f0bd65f1303b48b84
Jochen Ulrich <jochen.ulrich@gameforge.com>
2017-02-23 23:53:59 +0100

    Extends DeferredTest

--- tests/Deferred/DeferredTest.cpp
+++ tests/Deferred/DeferredTest.cpp
@@ -16,6 +16,7 @@ private slots:
 	void resolveTest();
 	void rejectTest();
 	void notifyTest();
+	void destructorTest();
 
 private:
 	struct DeferredSpies
@@ -63,16 +64,79 @@ void DeferredTest::resolveTest()
 	QCOMPARE(spies.rejected.count(), 0);
 	QCOMPARE(spies.notified.count(), 0);
 	QCOMPARE(spies.resolved.first().first().toString(), value);
+
+	QVERIFY(!deferred->notify(QString("progress")));
+	QVERIFY(!deferred->reject(QString("reason")));
+	QCOMPARE(deferred->data().toString(), value);
+	QCOMPARE(deferred->state(), Deferred::Resolved);
+	QCOMPARE(spies.resolved.count(), 1);
+	QCOMPARE(spies.rejected.count(), 0);
+	QCOMPARE(spies.notified.count(), 0);
 }
 
 void DeferredTest::rejectTest()
 {
+	Deferred::Ptr deferred = Deferred::create();
 
+	DeferredSpies spies(deferred);
+
+	QString value("myValue");
+
+	QVERIFY(deferred->reject(value));
+	QCOMPARE(deferred->data().toString(), value);
+	QCOMPARE(deferred->state(), Deferred::Rejected);
+	QCOMPARE(spies.resolved.count(), 0);
+	QCOMPARE(spies.rejected.count(), 1);
+	QCOMPARE(spies.notified.count(), 0);
+	QCOMPARE(spies.rejected.first().first().toString(), value);
+
+	QVERIFY(!deferred->notify(QString("progress")));
+	QVERIFY(!deferred->resolve(QString("value")));
+	QCOMPARE(deferred->data().toString(), value);
+	QCOMPARE(deferred->state(), Deferred::Rejected);
+	QCOMPARE(spies.resolved.count(), 0);
+	QCOMPARE(spies.rejected.count(), 1);
+	QCOMPARE(spies.notified.count(), 0);
 }
 
 void DeferredTest::notifyTest()
 {
+	Deferred::Ptr deferred = Deferred::create();
 
+	DeferredSpies spies(deferred);
+
+	QString firstValue("myValue");
+
+	QVERIFY(deferred->notify(firstValue));
+	QVERIFY(deferred->data().isNull());
+	QCOMPARE(deferred->state(), Deferred::Pending);
+	QCOMPARE(spies.resolved.count(), 0);
+	QCOMPARE(spies.rejected.count(), 0);
+	QCOMPARE(spies.notified.count(), 1);
+	QCOMPARE(spies.notified.at(0).first().toString(), firstValue);
+
+	int secondValue(3);
+
+	QVERIFY(deferred->notify(secondValue));
+	QVERIFY(deferred->data().isNull());
+	QCOMPARE(deferred->state(), Deferred::Pending);
+	QCOMPARE(spies.resolved.count(), 0);
+	QCOMPARE(spies.rejected.count(), 0);
+	QCOMPARE(spies.notified.count(), 2);
+	QCOMPARE(spies.notified.at(1).first().toInt(), secondValue);
+}
+
+void DeferredTest::destructorTest()
+{
+	Deferred::Ptr deferred = Deferred::create();
+
+	DeferredSpies spies(deferred);
+
+	deferred.clear();
+
+	QVERIFY(spies.rejected.wait()); // Need an event loop to execute deleteLater
+	QCOMPARE(spies.rejected.count(), 1);
+	QVERIFY(spies.rejected.first().first().canConvert<DeferredDestroyed>());
 }
 
 
