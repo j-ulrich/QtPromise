229f72ad3312e558fb651ae297becc05d38636fd
Jochen Ulrich <jochen.ulrich@gameforge.com>
2017-02-23 18:36:53 +0100

    Extends PromiseTest

--- tests/Promise/PromiseTest.cpp
+++ tests/Promise/PromiseTest.cpp
@@ -1,5 +1,6 @@
 
 #include <QtTest>
+#include "Promise.h"
 
 namespace QtPromise
 {
@@ -11,13 +12,30 @@ class PromiseTest : public QObject
 	Q_OBJECT
 
 private slots:
+	void createResolvedPromiseTest();
+	void createRejectedPromiseTest();
 
 };
 
 
+void PromiseTest::createResolvedPromiseTest()
+{
+	QString myString = "String";
+	Promise::Ptr promise = Promise::createResolved(myString);
+
+	QCOMPARE(promise->state(), Deferred::Resolved);
+}
+
+void PromiseTest::createRejectedPromiseTest()
+{
+	QString myString = "String";
+	Promise::Ptr promise = Promise::createRejected(myString);
+
+	QCOMPARE(promise->state(), Deferred::Rejected);
+}
+
 }  // namespace Tests
 }  // namespace QtPromise
-
 
 
 QTEST_MAIN(QtPromise::Tests::PromiseTest)
